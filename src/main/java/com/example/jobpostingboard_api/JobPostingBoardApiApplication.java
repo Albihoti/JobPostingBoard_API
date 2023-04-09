6c6d8c0caa70f8a71296b7b56f458d1f892bccf0
Albihoti <100148161+Albihoti@users.noreply.github.com>
2023-04-09 02:23:14 +0200

    Initialized project

---
+++ src/main/java/com/example/jobpostingboard_api/JobPostingBoardApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.jobpostingboard_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobPostingBoardApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JobPostingBoardApiApplication.class, args);
+    }
+
+}
