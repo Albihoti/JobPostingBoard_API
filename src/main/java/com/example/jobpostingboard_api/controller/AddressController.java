6661933af24ae8326089e0608ba08ee9709d231f
Albihoti <100148161+Albihoti@users.noreply.github.com>
2023-04-10 23:00:43 +0200

    feature: address endpoints

--- src/main/java/com/example/jobpostingboard_api/controller/AddressController.java
+++ src/main/java/com/example/jobpostingboard_api/controller/AddressController.java
@@ -6,10 +6,9 @@ import com.example.jobpostingboard_api.entity.Address;
 import com.example.jobpostingboard_api.service.AddressService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/address")
@@ -22,4 +21,29 @@ public class AddressController {
     public ResponseEntity<Address> addNewAddress(@RequestBody AddressRequestDto addressRequestDto){
         return ResponseEntity.ok(addressService.addNewAddress(addressRequestDto));
     }
+
+    @GetMapping("/all")
+    public  ResponseEntity<List<Address>> getAllAddresses(){
+        return ResponseEntity.ok(addressService.getAllAddresses());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Address> getAddressById(@PathVariable("id") int id){
+        return ResponseEntity.ok(addressService.findAddressById(id));
+    }
+
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Address> updateAddressById(@PathVariable("id") int id, @RequestBody AddressRequestDto addressRequestDto){
+        return ResponseEntity.ok(addressService.updateAddressById(id, addressRequestDto));
+    }
+
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteAddressById(@PathVariable int id){
+        return ResponseEntity.ok(addressService.deleteAddressById(id));
+
+    }
+
+
 }
--- src/main/java/com/example/jobpostingboard_api/service/AddressService.java
+++ src/main/java/com/example/jobpostingboard_api/service/AddressService.java
@@ -7,6 +7,7 @@ import com.example.jobpostingboard_api.repository.AddressRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -28,10 +29,30 @@ public class AddressService {
 
         return address;
 
-
     }
 
     public Address findAddressById(Integer id){
        return addressRepository.findById(id).orElse(null);
+    }
+
+    public List<Address> getAllAddresses() {
+        return addressRepository.findAll();
+    }
+
+    public Address updateAddressById(int id, AddressRequestDto addressRequestDto) {
+        var address = findAddressById(id);
+        address.setZipCode(addressRequestDto.getZipCode());
+        address.setStreet(addressRequestDto.getStreet());
+        address.setState(addressRequestDto.getState());
+        address.setCity(addressRequestDto.getCity());
+
+        addressRepository.save(address);
+        return address;
+    }
+
+    public String deleteAddressById(int id) {
+
+        addressRepository.deleteById(id);
+        return "Address deleted successfully";
     }
 }
